bf8da856eda1ae3146df7a34b19287691e22a0da
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-20 16:50:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
 def introduction(name)
-  "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 introduction(name)
 
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction(name, language)
